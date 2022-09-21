ceb55ef6a147adc0c54231bc648707d01e863839
zhouxu <37956613+zhouxu-github@users.noreply.github.com>
2022-09-21 10:27:30 +0800

    Add files via upload

---
+++ C++提高编程/模板/类模板/Difference_between_class_template_and_function_template/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+//类模板与函数模板的区别
+
+template<class NameType, class AgeType=int>
+class Person {
+public:
+    Person(NameType name, AgeType age) {
+        this->m_Name = name;
+        this->m_Age = age;
+    }
+
+    void showPerson() {
+        cout << "name = " << this->m_Name << " age = " << this->m_Age << endl;
+    }
+
+    NameType m_Name;
+    AgeType m_Age;
+};
+
+//1.类模板没有自动类型推导使用方式
+void test01() {
+//    Person p("孙悟空",1000); 错误，无法用自动类型推导
+    Person<string, int> p("孙悟空", 1000);
+    p.showPerson();
+}
+
+//2.类模板在模板参数列表中可以有默认参数
+void test02() {
+    Person<string> p("猪八戒", 999);
+    p.showPerson();
+}
+
+int main() {
+
+    test01();
+
+    test02();
+
+    return 0;
+}
